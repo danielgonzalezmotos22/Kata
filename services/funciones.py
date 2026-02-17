af115d327118ffba157ebc7cad622fb560b95599
Janhjg <janhjanh93@gmail.com>
2026-02-17 11:31:05 +0100

    test bonitos

--- services/funciones.py
+++ services/funciones.py
@@ -1,5 +1,8 @@
-def calcular(cadena: str) -> int:
-    if not cadena:
+def calcular(numeros: str) -> int:
+    if not numeros:
         return 0
-    numeros = [int(n) for n in cadena.split(',')]
+    numeros = [int(n) for n in numeros.split(',')]
     return sum(numeros)
+
+if __name__ == '__main__':
+    print(calcular('1,2,3,5,10,10')) 
--- test/test_funciones.py
+++ test/test_funciones.py
@@ -1,8 +1,11 @@
 from services.funciones import *
 
-def test_calcular():
-    assert calcular('') == 0
-    assert calcular('0') == 0
-    assert calcular('1') == 1
-    assert calcular('1,2') == 3
+
+def test_vacio_y_0():
+    assert calcular('') == 0 #si la lista esta vacia
+    assert calcular('0') == 0 #si te da o
+def test_1():
+    assert calcular('1') == 1 
+def test_suma_varios():
+    assert calcular('1,2') == 3 
     assert calcular('1,2,3') == 6
